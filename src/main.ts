9d95c3c0bc1b281e666c729607462d766c6735eb
Nanukh <dylan.koehl@protonmail.com>
2023-12-06 17:54:33 +0100

    Update banner again

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ console.log(`
   _______________________________________________________________________
                               |  
   Stars collected:  12 / 50   |                            Code by Nanukh
-  ****.....................   |                 https://github.com/Nanukh
+  ******...................   |                 https://github.com/Nanukh
   `)
 setTimeout(() => {
     challenges.forEach((c: BaseChallenge) => c.run())
